869b30cf08bb6738dd13675d563c1a2f181ceb38
okanon <flat.cydia37jins.pc@gmail.com>
2020-10-28 03:20:07 +0900

    import Foundation

--- src/NSString-CDExtensions.h
+++ src/NSString-CDExtensions.h
@@ -3,6 +3,9 @@
 //  This file is part of class-dump, a utility for examining the Objective-C segment of Mach-O files.
 //  Copyright (C) 1997-2019 Steve Nygard.
 
+#import <Foundation/Foundation.h>
+
+
 @interface NSString (CDExtensions)
 
 + (NSString *)stringWithFileSystemRepresentation:(const char *)str;
